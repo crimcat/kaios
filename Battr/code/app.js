1939f7af159b5644d32fa4033c34760b317d947e
Stas Torgashov <crimcat@yandex.ru>
2018-08-30 23:54:35 +0300

    More readable charging/discharging time

--- Battr/code/app.js
+++ Battr/code/app.js
@@ -2,48 +2,50 @@
 
 window.addEventListener("load", function() {
 
-    Number.prototype.pad = function(size) {
-        var s = String(this);
-        while(s.length < (size || 2)) {
-            s = "0" + s;
-        }
-        return s;
-    }
-
     function refreshBatteryStatus() {
         navigator.getBattery().then(function(battery) {
 
-            document.querySelector('#level').textContent = '' + battery.level * 100 + '%';
-            document.querySelector('#health').textContent = battery.health;
+            document.querySelector("#level").textContent = "" + battery.level * 100 + "%";
+            document.querySelector("#health").textContent = battery.health;
 
-            document.querySelector('#charging').textContent = battery.charging ? 'yes' : 'no';
+            document.querySelector("#charging").textContent = battery.charging ? "yes" : "no";
 
             var batteryTime = battery.charging ? battery.chargingTime : battery.dischargingTime;
-            var prefix = battery.charging ? 'Time to charge:  ' : 'Time to discharge:  ';
+            var prefix = battery.charging ? "Charging time:  " : "Remaining time:  ";
 
             if(isFinite(batteryTime)) {
-                var ch = Math.floor(batteryTime / 3600);
-                var paddingZeros = ch > 100 ? 3 : 2;
-                var cm = Math.floor(batteryTime % 3600 / 60);
-                document.querySelector('#batteryTiming').textContent = prefix + (ch).pad(paddingZeros) + ':' + (cm).pad(2);
+                var timingStr = prefix;
+                var minutes = Math.floor(batteryTime / 60);
+                if(minutes < 60) {
+                    timingStr += "" + minutes + " min";
+                } else {
+                    var hours = Math.floor(minutes / 60);
+                    timingStr += "" + hours;
+                    if(minutes % 60 > 30) {
+                        timingStr += ".5 hours";
+                    } else {
+                        timingStr += hours > 1 ? " hours" : "hour";
+                    }
+                }
+                document.querySelector("#batteryTiming").textContent = timingStr;
             } else {
-                document.querySelector('#batteryTiming').textContent = prefix + 'n/a';
+                document.querySelector("#batteryTiming").textContent = prefix + "n/a";
             }
 
-            document.querySelector('#temperature').textContent = '' + battery.temperature + '°C';
+            document.querySelector('#temperature').textContent = "" + battery.temperature + "°C";
         });
 
     }
 
     function handleKeyDown(e) {
         switch(e.key) {
-            case 'Enter':
+            case "Enter":
                 refreshBatteryStatus();
             break;
         }
     }
 
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener("keydown", handleKeyDown);
 
     refreshBatteryStatus();
 });
